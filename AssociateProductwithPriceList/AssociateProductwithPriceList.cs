742ab047b8fe1c16352200e9a3327319c7d01381
Hadeel Alhamadah <halhamadah@BondConsultingServices.com>
2022-12-07 12:00:13 -0800

    test

--- AssociateProductwithPriceList/AssociateProductwithPriceList.cs
+++ AssociateProductwithPriceList/AssociateProductwithPriceList.cs
@@ -8,6 +8,7 @@ namespace AssociateProductwithPriceList
     {
         public void Execute(IServiceProvider serviceProvider)
         {
+            //test commit
             ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
 
